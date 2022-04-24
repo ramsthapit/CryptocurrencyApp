9c79c5e0fa9c4c308b93285929c11e21b2eccd7b
Ram Sthapit <ramsthapit50@gmail.com>
2022-04-24 17:11:05 +0545

    homepage updated

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,30 @@
+import { Col, Row, Statistic, Typography } from 'antd'
 import React from 'react'
+
+const { Title } = Typography
 
 const Homepage = () => {
   return (
-    <div>
-      <h1>Homepage</h1>
-    </div>
+    <>
+      <Title level={2} className="heading" >Global Crypto Stats</Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Exchanges" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market cap" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24 Volume" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Markets" value="5" />
+        </Col>
+      </Row>
+    </>
   )
 }
 
